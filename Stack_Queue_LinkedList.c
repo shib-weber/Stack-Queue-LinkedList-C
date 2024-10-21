d5aaac10dcf0708e8ad29b4f58f5b9720e5f46aa
shibsrc <shibjyoti252@gmail.com>
2024-10-21 22:33:21 +0530

    Stack Implementation

--- Stack_Queue_LinkedList.c
+++ Stack_Queue_LinkedList.c
@@ -1,11 +1,58 @@
 #include<stdio.h>
+void stack(){
+    int c,size,top=-1,n;
+    printf("Enter the Size of the Stack");
+    scanf("%d",&size);
+    int ar[size];
+    do{
+        printf("Enter Your Choice \n1 for push \n2 for pop \n3 for display \n4 for exit");
+        scanf("%d",&c);
+        switch(c){
+            case 1 :
+                printf("Enter a Number\t");
+                scanf("%d",&n);
+                if(top == size-1){
+                    printf("Stack Overflow");
+                }else{
+                    ar[++top] = n;
+                }
+            break;
+            case 2: 
+                if(top == -1){
+                    printf("Stack Underflow");
+                }else{
+                    printf("The Popped Element is %d",ar[top--]);
+                }
+            break;
+            case 3: 
+                if(top == -1){
+                    printf("Stack Underflow");
+                }else{
+                    for(int i =size-1 ;i>=0;i--){
+                        if(ar[i]==0){
+                            continue;
+                        }else{
+                            printf("%d\n",ar[i]);
+                        }
+                    }
+                }
+            break;
+            case 4: break;
+            default: printf("No other choice");
+            break;
+    }
+    }while(c != 4);
+
+}
 int main(){
     char ch;
     printf("Enter Your Choice : \n's' for Stack\n'q' for Queue \n'l' for Linked List\n");
     scanf("%c",&ch);
     switch (ch)
     {
-    case 's': printf("You have Chosen Stack");
+    case 's': 
+        printf("You have Chosen Stack\n");
+        stack();
         break;
     case 'q': printf("You have Chosen Queue");
         break;
