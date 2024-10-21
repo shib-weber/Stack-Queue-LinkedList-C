9750dad322aeb2eef2584c7dde81b11b2b2f48ea
shibsrc <shibjyoti252@gmail.com>
2024-10-21 21:39:30 +0530

    Choice section Completed

--- Stack_Queue_LinkedList.c
+++ Stack_Queue_LinkedList.c
@@ -3,6 +3,16 @@ int main(){
     char ch;
     printf("Enter Your Choice ");
     scanf("%c",&ch);
-    printf("Your choice is %c",ch);
+    switch (ch)
+    {
+    case 's': printf("You have Chosen Stack");
+        break;
+    case 'q': printf("You have Chosen Queue");
+        break;
+    case 'l': printf("You have Chosen LinkedList");
+        break;
+    default:printf("o Other Choice");
+        break;
+    }
     return 0;
 }
