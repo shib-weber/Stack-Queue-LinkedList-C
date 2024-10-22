c18baed466435eae4e29ce83b8dc1d8eeb21d4f0
Shibjyoti-src <139529315+shib-weber@users.noreply.github.com>
2024-10-22 08:46:38 +0530

    Update Stack_Queue_LinkedList.c

--- Stack_Queue_LinkedList.c
+++ Stack_Queue_LinkedList.c
@@ -44,6 +44,55 @@ void stack(){
     }while(c != 4);
 
 }
+
+void queue(){
+    int size;n,f=-1,r=-1;
+    printf("Enter the size of the Queue \n");
+    scanf("%d",&size);
+        int ar[size];
+    do{
+        printf("Enter Your Choice \n1 for Enqueue \n2 for Dequeue \n3 for Display \n4 for exit\n");
+        scanf("%d",&c);
+        switch(c){
+            case 1 :
+                if(f==-1&&r==-1){
+                    
+                }
+                printf("Enter a Number\t");
+                scanf("%d",&n);
+                if(top == size-1){
+                    printf("Stack Overflow\n");
+                }else{
+                    ar[++top] = n;
+                }
+            break;
+            case 2: 
+                if(top == -1){
+                    printf("Stack Underflow\n");
+                }else{
+                    printf("The Popped Element is %d\n",ar[top--]);
+                }
+            break;
+            case 3: 
+                if(top == -1){
+                    printf("Stack Underflow\n");
+                }else{
+                    for(int i =size-1 ;i>=0;i--){
+                        if(ar[i]==0){
+                            continue;
+                        }else{
+                            printf("%d\n",ar[i]);
+                        }
+                    }
+                }
+            break;
+            case 4: break;
+            default: printf("No other choice");
+            break;
+    }
+    }while(c != 4);
+}
+
 int main(){
     char ch;
     printf("Enter Your Choice : \n's' for Stack\n'q' for Queue \n'l' for Linked List\n");
