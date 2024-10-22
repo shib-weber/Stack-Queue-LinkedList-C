01e1d90f89660692d501dfbbfd06c1b5f3463df5
shibsrc <shibjyoti252@gmail.com>
2024-10-23 00:47:58 +0530

    Circular Queue Added

--- Stack_Queue_LinkedList.c
+++ Stack_Queue_LinkedList.c
@@ -238,10 +238,76 @@ void linkedlist(){
     
 }
 
+void circularQ(){
+    int size,n,c,f=-1,r=-1;
+    printf("Enter the size of The Queue\n");
+    scanf("%d",&size);
+    int ar[size];
+    do{
+        printf("Enter Your Choice\n1 for Enqueue\n2 for Dequeue\n3 for Display\n4 for Exit\n");
+        scanf("%d",&c);
+        switch(c){
+            case 1:
+            if((r==(f-1)%(size-1)) || (f==0 && r ==size-1)){
+                printf("Queue is Full\n");
+            }else{
+                printf("Enter a Number\t");
+                scanf("%d",&n);
+                    if (f == -1) { // First element to insert
+                        f = r = 0;
+                        ar[r] = n;
+                    } else if (r == size - 1 && f != 0) {
+                        r = 0;
+                        ar[r] = n;
+                    } else {
+                        r++;
+                        ar[r] = n;
+                    }
+            }
+            break;
+            case 2:
+            if(f==-1 && r==-1){
+                printf("Queue is Empty\n");
+            }else{
+                
+                printf("Dequeued Element is %d\n",ar[f]);
+                if (f == r) { // Queue has only one element
+                    f = r = -1;
+                } else if (f == size - 1) {
+                    f = 0;
+                } else {
+                    f++;
+                }
+            }
+            break;
+            case 3:
+                if(f==-1 && r==-1){
+                    printf("Queue is Empty\n");
+                }else{
+                    if (r >= f) {
+                        for (int i = f; i <= r; i++) {
+                        printf("%d ", ar[i]);
+                    }
+                    } else {
+                        for (int i = f; i < size; i++) {
+                        printf("%d ", ar[i]);
+                    }
+                        for (int i = 0; i <= r; i++) {
+                        printf("%d ", ar[i]);
+                    }
+                }
+                    printf("\n");
+                }
+                break;
+                case 4:break;
+                default:printf("No other Choice\n");break;
+        }
+    }while(c!=4);
+}
 
 int main(){
     char ch;
-    printf("Enter Your Choice : \n's' for Stack\n'q' for Queue \n'l' for Linked List\n");
+    printf("Enter Your Choice : \n's' for Stack\n'q' for Queue \n'l' for Linked List\n'c' for Circular Queue\n");
     scanf("%c",&ch);
     switch (ch)
     {
@@ -257,7 +323,11 @@ int main(){
         printf("You have Chosen LinkedList\n");
         linkedlist();
         break;
-    default:printf("o Other Choice");
+    case 'c':
+        printf("You have Chosen Circular Queue\n");
+        circularQ();
+        break;
+    default:printf("No Other Choice");
         break;
     }
     return 0;
